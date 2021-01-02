3cea38a18bf765eee50c35f2a2c0409fc7393716
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2021-01-02 07:58:44 +0000

    Done.

--- index.js
+++ index.js
@@ -27,30 +27,3 @@ document.addEventListener("keydown", function(e) {
     moveDodgerRight();
   }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function moveDodgerRight() {
-//   let leftNumbers = dodger.style.left.replace("px", "");
-//   let left = parseInt(leftNumbers, 10);
-//
-//   if (left < 180) {
-//     dodger.style.left = `${left + 1}px`;
-//   }
-// }
-// document.addEventListener("keydown", function(e) {
-//   if (e.key === "ArrowLeft") {
-//     moveDodgerRight();
-//   }
-// });
